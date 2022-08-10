f3e784ec39a464362089b226c267d60ede58418b
yhi <yhi@java>
2022-08-10 16:36:07 +0900

    두번째 버전
    최초시작시 소개화면 추가

--- login/src/login/Main.java
+++ login/src/login/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("로그인을 시작하지!");
 		String id,pw;
 		Scanner sc = new Scanner(System.in);
 		Check check = new Check();
